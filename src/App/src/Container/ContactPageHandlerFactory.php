89a1c8da46e5015ef3994432797a8a0fb75562d6
Joey Smith <jsmith@webinertia.net>
2026-02-23 12:57:58 -0600

    coding-standard fixes Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/App/src/Container/ContactPageHandlerFactory.php
+++ src/App/src/Container/ContactPageHandlerFactory.php
@@ -2,6 +2,16 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of the Tyrsson Webinertia package.
+ *
+ * Copyright (c) 2026 Joey Smith <jsmith@webinertia.net>
+ * and contributors.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Container;
 
 use App\Handler\ContactPageHandler;
@@ -13,6 +23,7 @@ final class ContactPageHandlerFactory
     public function __invoke(ContainerInterface $container): ContactPageHandler
     {
         $template = $container->get(TemplateRendererInterface::class);
+
         return new ContactPageHandler($template);
     }
 }
